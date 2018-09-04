2c613bf9cdafd8dbb2a1bf24e2613a259c693dea
sanchitnegi <sanchit.negi@edfora.com>
2018-09-04 13:52:35 +0530

    Second Changes

--- routes/users/signup.js
+++ routes/users/signup.js
@@ -5,7 +5,7 @@ module.exports = function (router) {
     router.post('/signUp',userSignUpController.signUp);
     router.get('/testing',userSignUpController.testing);
     router.post('/updateUser',middleware.middleware,userSignUpController.updateUser);
-    router.get('/', function(req, res) {
+     router.get('/', function(req, res) {
             res.sendfile('index.html');
     });
 
